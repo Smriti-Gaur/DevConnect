4c80c4ec86b7f03ce9483430b58b503d54c0d72b
Smriti Gaur <smritigaur2105@gmail.com>
2025-07-17 22:02:32 +0530

    Initial Commit: Set up Base of the project.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+      <h1  class="text-4xl font-bold text-gray-900 bg-blue-500" >DevConnnect.....</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   // Add your content paths here
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}", // Adjust this based on your project structure
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'), // This line is crucial for DaisyUI
+//   ],
+
+//   // Optional: DaisyUI specific configuration
+//   daisyui: {
+//     themes: ["light", "dark", "cupcake", "emerald", "synthwave"], // Add themes you want to use
+//     // Add other DaisyUI config options here if needed
+//   },
+// }
